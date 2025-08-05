d18ea3a7599378fcb8b8c33f45affb1c454ab830
Backmeet <pankajmaini09@gmail.com>
2025-08-05 09:43:02 +0530

    made it non-dependet

--- ROS/CLI.py
+++ ROS/CLI.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.insert(0, r"../../ver1.2")
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "ver1.2"))
 
 from ruby import runRuby, StableHash
 
